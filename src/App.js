7bb134e0388436db9528b7bc575f11a5532933b6
shakilla1 <shakilshifat65@gmail.com>
2022-09-26 11:43:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './componends/Header/Header';
+import Shop from './componends/Shop/Shop';
+
+const App = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Shop></Shop>
+        </div>
+    );
+};
+
+export default App;
